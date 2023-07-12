e02e3efa7cac6cd32c37ebdf6e15337463a45504
surfacez <sergiobatistajr94@gmail.com>
2023-07-12 11:06:10 -0300

    adding filters

---
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -0,0 +1,66 @@
+import getCategory from "@/actions/get-category";
+import getColors from "@/actions/get-colors";
+import getProducts from "@/actions/get-products";
+import getSizes from "@/actions/get-sizes";
+
+import ProductCard from "@/components/ui/product-card";
+import NoResult from "@/components/ui/no-results";
+import Container from "@/components/ui/container";
+import Billboard from "@/components/billboard";
+
+import MobileFilters from "./components/mobile-filters";
+import Filter from "./components/filter";
+
+export const revalidate = 0;
+
+interface CategoryPageProps {
+  params: {
+    categoryId: string;
+  };
+  searchParams: {
+    colorId: string;
+    sizeId: string;
+  };
+}
+
+const CategoryPage: React.FC<CategoryPageProps> = async ({
+  params,
+  searchParams,
+}) => {
+  const products = await getProducts({
+    categoryId: params.categoryId,
+    colorId: searchParams.colorId,
+    sizeId: searchParams.sizeId,
+  });
+  const colors = await getColors();
+  const sizes = await getSizes();
+  const category = await getCategory(params.categoryId);
+  console.log(products);
+
+  return (
+    <div className="bg-white">
+      <Container>
+        <Billboard data={category.billboard} />
+      </Container>
+      <div className="px-4 sm:px-6 lg:px-8 pb-24">
+        <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+          <MobileFilters sizes={sizes} colors={colors} />
+          <div className="hidden lg:block ">
+            <Filter valueKey="sizeId" name="Sizes" data={sizes} />
+            <Filter valueKey="colorId" name="Colors" data={colors} />
+          </div>
+          <div className="mt-6 lg:col-span-4 lg:mt-0">
+            {products.length === 0 && <NoResult />}
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+              {products.map((item) => (
+                <ProductCard key={item.id} data={item} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryPage;
